860508d3c10eeab70907a10aff8aacaffea374be
Solomon <solomonshuga@gmail.com>
2019-08-14 13:50:45 +0100

    SimpleInterest

---
+++ src/s/i/SI.java
@@ -0,0 +1,12 @@
+
+public class SI {
+
+    public static void main(String[] args) {
+     float p, r, t, si; 
+p =20000; r = 2; t = 25;
+si = (p*r*t)/100;
+System.out.println("Simple Interest is: " +si);
+}}
+    
+    
+  
